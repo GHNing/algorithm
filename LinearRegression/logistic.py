484453bb2ed5abdc864ba1392205ce43737a5270
3681lsn <2801077273@qq.com>
2019-05-12 11:27:39 +0800

    logistic 0512

--- LinearRegression/logistic.py
+++ LinearRegression/logistic.py
@@ -7,24 +7,51 @@ import warnings
 warnings.filterwarnings('ignore')
 
 class LR(object):
-    def __init__(self,rate = 0.01,iterations=500):
+    def __init__(self,rate = 0.01,iterations=500,):
         self.rate = rate
         self.iters = iterations
 
     def sigmoid(self,a):
         return 1.0/(1+np.exp(-a))
 
+    def Likelihood_value(self,x,y,paras):
+        data = pd.concat([x,y],axis=1)
+        count = len(data)
+        col = data.columns
+        end_value = 1
+
+        for m,n in data.iterrows():
+            flag = n[col[-1]]
+            p = np.mat(n[col[:-1]]) * paras.T
+            hx = self.sigmoid(p.tolist()[0][0])
+            if flag == 0.0:
+                hx = 1 - hx
+                end_value *= hx
+            else:
+                end_value *= hx
+
+        return end_value
+
     def run(self,x,y):
+        max = 0
         x_matrix = np.mat(x)
         y_matrix = np.mat(y).T
         rows,cols = np.shape(x)
         paras = np.ones((cols,1)).transpose()
-        for i in range(self.iters):
+        model_panas = np.zeros((cols,1))
+        for i in range(10):
             v1 = self.sigmoid(x_matrix * paras.T)
-            error = y_matrix - v1
+            error = v1 - y_matrix
             # print (error)
             paras = paras - self.rate * error.T * x_matrix
-            print(paras)
+            #迭代是否停止
+            tmp = self.Likelihood_value(x,y,paras)
+            print(tmp)
+            if max > tmp:
+                break
+            else:
+                max = tmp
+                model_panas = paras
 
 if __name__=='__main__':
     data = datasets.load_iris()
@@ -33,5 +60,15 @@ if __name__=='__main__':
     # print(df_train.head())
     # clf =  LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial').fit(df_train, Y)
     # print(clf.get_params())
+
+    t1 = pd.concat([df_train,Y],axis=1)
+    t2 = t1.loc[(t1[0] == 0) | (t1[0] == 1),:]
+    # print(t2[0].value_counts())
+    df_train = t2.iloc[:,:-1]
+    Y = t2[0]
+
     tmp = LR()
     tmp.run(df_train,Y)
+
+    # para = np.ones((1,4))
+    # print(tmp.Likelihood_value(df_train,Y,para))
